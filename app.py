f0d106bec09772847398681cb244ad61aba77caf
Yara-Glez <yajairagnzl11@gmail.com>
2024-11-23 12:47:45 -0700

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,7 @@ st.balloons()
 # Show title and description.
 st.title("💬 mi app")
 st.write(
-   "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
-   "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-   "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
+   "Hola bienvenido/a a la mejor experiencia.💕👌"
 )
 openai_api_key = st.secrets["api_key"] 
 # Create an OpenAI client.
